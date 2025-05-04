c4d9afe6a42d2edb354c1094d2135339def7d871
Tonywhite1997 <tonywhite1997814.tw@gmail.com>
2025-05-04 15:26:32 +0100

    back to free vercel

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -49,7 +49,7 @@ function Profile() {
   return `${year}/${month}/${day} - ${hour}:${minute}`;
 }
 
-const UI_URL = import.meta.env.VITE_APP_ENV === "development"? "http://localhost:5173" : "https://www.checksgiftcard.com"
+  const UI_URL = import.meta.env.VITE_APP_ENV === "development" ? "http://localhost:5173" : "https://apple-client-ecru.vercel.app"
 
 async function markAsRead(e, cardID){
   try{
