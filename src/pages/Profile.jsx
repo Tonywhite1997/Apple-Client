3b230ea6f6d153b70b970d57dd79a03ade7daa6b
Tonywhite1997 <tonywhite1997814.tw@gmail.com>
2025-05-04 08:54:49 +0100

    domain added to vercel

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -49,8 +49,7 @@ function Profile() {
   return `${year}/${month}/${day} - ${hour}:${minute}`;
 }
 
-const UI_URL = import.meta.env.VITE_APP_ENV === "development"? "http://localhost:5173" : "https://apple-client-ecru.vercel.app"
-// "https://checksgiftcard.com"
+const UI_URL = import.meta.env.VITE_APP_ENV === "development"? "http://localhost:5173" : "https://checksgiftcard.com"
 
 async function markAsRead(e, cardID){
   try{
