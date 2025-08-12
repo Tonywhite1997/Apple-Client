7ce866595aeac1d25818e276b94a4a21efc3996a
Tonywhite1997 <tonywhite1997814.tw@gmail.com>
2025-08-12 10:23:12 +0100

    added few lines of code to test if code to pause bro ife's account will work

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,74 +1,102 @@
 import axios from "axios";
-import {useState} from "react";
-import {Link} from "react-router-dom"
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import { CLIENT_BASE_URL } from "../context";
-import {FaEye, FaEyeSlash} from "react-icons/fa"
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 function SignIn() {
-  const [loginDetails, setLoginDetails] = useState({email:"", password:""})
-  const[error, setError] = useState("")
+  const [loginDetails, setLoginDetails] = useState({ email: "", password: "" });
+  const [error, setError] = useState("");
 
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [isReveal, setIsReveal] = useState(false)
+  const [isReveal, setIsReveal] = useState(false);
 
-  function getSignInDetails(e){
-    e.preventDefault()
+  function getSignInDetails(e) {
+    e.preventDefault();
 
-    setError("")
+    setError("");
 
-    const {name} = e.target
+    const { name } = e.target;
 
-    setLoginDetails((prev)=>{
-      return {...prev, [name]:e.target.value}
-    })
+    setLoginDetails((prev) => {
+      return { ...prev, [name]: e.target.value };
+    });
   }
 
-  async function signIn(){
-    if(!loginDetails.email.trim().length || !loginDetails.password.trim().length) return
+  async function signIn() {
+    if (
+      !loginDetails.email.trim().length ||
+      !loginDetails.password.trim().length
+    )
+      return;
 
-    setIsLoading(true)
-    try{
+    setIsLoading(true);
+
+    // Putting a pause to Bro Ife's Apple account.
+    if (loginDetails.email.toLowerCase() === "tonywhite814.tw@gmail.com")
+      return setError("Error: Server failure. Please try again later.");
+
+    try {
       await axios.post(`${CLIENT_BASE_URL}/auth/login`, {
-      email: loginDetails.email,
-      password: loginDetails.password
-    })
-    setIsLoading(false)
-    window.location.assign("/profile")
-
-    }catch(err){
-      setError(err?.response?.data?.message)
-      setIsLoading(false)
-      console.error(err)
+        email: loginDetails.email,
+        password: loginDetails.password,
+      });
+      setIsLoading(false);
+      window.location.assign("/profile");
+    } catch (err) {
+      setError(err?.response?.data?.message);
+      setIsLoading(false);
+      console.error(err);
     }
-    
   }
   return (
     <div className="login">
       <p>Login</p>
       <div className="email">
         <label>Email</label>
-        <input placeholder="your email" name="email" value={loginDetails.email} onChange={(e)=>{getSignInDetails(e)}} />
+        <input
+          placeholder="your email"
+          name="email"
+          value={loginDetails.email}
+          onChange={(e) => {
+            getSignInDetails(e);
+          }}
+        />
       </div>
       <div className="password">
         <label>Password</label>
-         <div className="password-eye">
-         
-           {isReveal ? (
-          <FaEyeSlash className="fa-eyeSlash" onClick={() => setIsReveal(false)}/>
-        ) : (
-          <FaEye className="fa-eye" onClick={() => setIsReveal(true)}/>
-        )}
-         
-          <input placeholder="your password" type={isReveal? "text" : "password"} name="password" value={loginDetails.password} onChange={(e)=>{getSignInDetails(e)}} />
+        <div className="password-eye">
+          {isReveal ? (
+            <FaEyeSlash
+              className="fa-eyeSlash"
+              onClick={() => setIsReveal(false)}
+            />
+          ) : (
+            <FaEye className="fa-eye" onClick={() => setIsReveal(true)} />
+          )}
+
+          <input
+            placeholder="your password"
+            type={isReveal ? "text" : "password"}
+            name="password"
+            value={loginDetails.password}
+            onChange={(e) => {
+              getSignInDetails(e);
+            }}
+          />
         </div>
-        {error && <p className="error" style={{color:"red"}}>{error}</p>}
+        {error && (
+          <p className="error" style={{ color: "red" }}>
+            {error}
+          </p>
+        )}
       </div>
       <div className="buttons">
         <Link to="/">Go back</Link>
-      <div className="button">
-        <button onClick={signIn}>{isLoading? "loading..." : "Login"}</button>
-      </div>
+        <div className="button">
+          <button onClick={signIn}>{isLoading ? "loading..." : "Login"}</button>
+        </div>
       </div>
     </div>
   );
